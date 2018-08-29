3a2f0aa56e173882759e579f2a11f6a0e2274130
RAMON NASCIMENTO <UC14100797@SLAB.UCB.BR>
2018-08-29 10:49:05 -0300

    update

---
+++ ExampleListView/app/src/main/java/com/example/uc14100797/examplelistview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.uc14100797.examplelistview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
