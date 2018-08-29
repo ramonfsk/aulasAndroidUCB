8b103898a58f2ae3d1310ecd92576abbb6522fe3
RAMON NASCIMENTO <UC14100797@SLAB.UCB.BR>
2018-08-29 11:20:01 -0300

    update

--- ExampleListView/app/src/main/java/com/example/uc14100797/examplelistview/MainActivity.java
+++ ExampleListView/app/src/main/java/com/example/uc14100797/examplelistview/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.uc14100797.examplelistview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
+
+    ListView listView;
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arrayAdapter = new ArrayAdapter(
+                this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.arrayCars)
+        );
+
+        listView = findViewById(R.id.list);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this,"Um item foi clicado!", Toast.LENGTH_LONG).show();
     }
 }
